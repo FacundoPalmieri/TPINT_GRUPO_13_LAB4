86b1e5e0e4195481ed4cf01ca922a63f4dc4e43f
unknown <palmierifacundo@gmail.com>
2024-07-07 22:02:37 -0300

    Se avanza con cuentas

--- Proyecto/src/entidad/Movimientos.java
+++ Proyecto/src/entidad/Movimientos.java
@@ -4,12 +4,12 @@ import java.time.LocalDate;
 
 public class Movimientos {
 	private int id;
-	private int cuenta_origen;
+	private Cuenta cuenta_origen;
 	private LocalDate fecha;
 	private String detalle;
 	private float importe;
-	private int cuenta_destino;
-	private int tipo_Movimiento_id;
+	private Cuenta cuenta_destino;
+	private TipoMovimiento tipo_Movimiento_id;
 	
 	public int getId() {
 		return id;
@@ -17,10 +17,10 @@ public class Movimientos {
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getCuenta_origen() {
+	public Cuenta getCuenta_origen() {
 		return cuenta_origen;
 	}
-	public void setCuenta_origen(int cuenta_origen) {
+	public void setCuenta_origen(Cuenta cuenta_origen) {
 		this.cuenta_origen = cuenta_origen;
 	}
 	public LocalDate getFecha() {
@@ -41,16 +41,16 @@ public class Movimientos {
 	public void setImporte(float importe) {
 		this.importe = importe;
 	}
-	public int getCuenta_destino() {
+	public Cuenta getCuenta_destino() {
 		return cuenta_destino;
 	}
-	public void setCuenta_destino(int cuenta_destino) {
+	public void setCuenta_destino(Cuenta cuenta_destino) {
 		this.cuenta_destino = cuenta_destino;
 	}
-	public int getTipo_Movimiento_id() {
+	public TipoMovimiento getTipo_Movimiento_id() {
 		return tipo_Movimiento_id;
 	}
-	public void setTipo_Movimiento_id(int tipo_Movimiento_id) {
+	public void setTipo_Movimiento_id(TipoMovimiento tipo_Movimiento_id) {
 		this.tipo_Movimiento_id = tipo_Movimiento_id;
 	}
 	
